072c098e16409e07bd3539b072189f08951c80ed
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-09-05 00:03:38 +0300

    add consttans

--- src/Http/Exception.php
+++ src/Http/Exception.php
@@ -5,5 +5,10 @@ namespace NetworkTransport\Http;
 class Exception extends \Exception
 {
     public const BAD_REQUEST = 400;
+    public const NOT_FOUND = 404;
     public const METHOD_NOT_ALLOWED = 405;
+    public const CLIENT_TIMEOUT = 408;
+    public const INTERNAL_SERVER_ERROR = 500;
+    public const BAD_GATEWAY = 502;
+    public const SERVER_TIMEOUT = 504;
 }
