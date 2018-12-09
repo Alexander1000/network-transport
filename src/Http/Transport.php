c62d1061ac6e1a306c97a167665154207c524f9b
Aleksandr Dankovtsev <adankovtsev@avito.ru>
2018-12-09 12:24:34 +0300

    parallels

--- src/Http/Transport.php
+++ src/Http/Transport.php
@@ -6,7 +6,34 @@ use NetworkTransport;
 
 class Transport implements TransportInterface
 {
-    private $multiCurlResource;
+    /**
+     * @var int
+     */
+    private $parallelTaskId = 0;
+
+    /**
+     * @var resource[]
+     */
+    private $multiCurlResource = [];
+
+    private $requestCollector = [];
+
+    private $responseCollector = [];
+
+    /**
+     * @param Request $request
+     * @return $this
+     */
+    public function add(Request $request)
+    {
+        if (empty($this->multiCurlResource[$this->parallelTaskId])) {
+            $this->multiCurlResource[$this->parallelTaskId] = curl_multi_init();
+        }
+
+        curl_multi_add_handle($this->multiCurlResource[$this->parallelTaskId], $request->getResource());
+        $this->requestCollector[$this->parallelTaskId][$request->getHash()] = $request;
+        return $this;
+    }
 
     /**
      * @param Request $request
@@ -15,6 +42,29 @@ class Transport implements TransportInterface
      */
     public function send(Request $request): Response
     {
+        $active = null;
+        $mh = $this->multiCurlResource[$this->parallelTaskId];
+        do {
+            $mrc = curl_multi_exec($mh, $active);
+        } while($mrc == CURLM_CALL_MULTI_PERFORM);
+
+        while ($active && $mrc == CURLM_OK) {
+            if (curl_multi_select($mh) != -1) {
+                do {
+                    $mrc = curl_multi_exec($mh, $active);
+                } while ($mrc == CURLM_CALL_MULTI_PERFORM);
+            }
+        }
+
+        foreach ($this->requestCollector[$this->parallelTaskId] as $hash => $tRequest) {
+            /** @var Request $tRequest */
+            $this->responseCollector[$this->parallelTaskId][$hash] = curl_multi_getcontent($tRequest->getResource());
+            curl_multi_remove_handle($mh, $tRequest->getResource());
+        }
+
+        curl_multi_close($mh);
+        $this->parallelTaskId++;
+
         $ch = $request->getResource();
         $result = curl_exec($ch);
         if ($result === false) {
